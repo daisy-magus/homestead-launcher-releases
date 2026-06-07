77ca3cdbba525f40fb8dbf0d63c410e8f4631794
daisy-magus <daisy.magus@gmail.com>
2026-06-07 10:49:52 -0700

    Release v1.21.0

--- launcher/config.py
+++ launcher/config.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 from platformdirs import user_config_dir, user_data_dir
 
-VERSION = "1.20.0"
+VERSION = "1.21.0"
 
 APP_NAME   = "HomesteadLauncher"
 APP_AUTHOR = "Homestead"
